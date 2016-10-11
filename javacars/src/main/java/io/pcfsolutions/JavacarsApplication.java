52b99f5e5404c084337c7967349708b3539a7b88
benwilcock <benwilcock@gmail.com>
2016-10-11 14:16:06 +0100

    Added javacars and added the MySQL DB properties required to boot the server with a datasource.

---
+++ javacars/src/main/java/io/pcfsolutions/JavacarsApplication.java
@@ -0,0 +1,12 @@
+package io.pcfsolutions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavacarsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavacarsApplication.class, args);
+	}
+}
